cd003634fa069453188a433ebc1b52c6475d536b
Elias Turbay <eliasturbay@gmail.com>
2019-01-22 12:55:11 -0300

    Initial commit

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "expaf6406227bdc4ba6b9de2bac0092205b://192.168.1.5:19000";
+
+}
